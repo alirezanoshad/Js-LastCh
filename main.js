a7a7738a8cacb1d8b8d48540c8bf62bb28b07f58
Alireza <iranexplosm@gmail.com>
2024-08-11 16:29:01 +0330

    -js file completed

---
+++ main.js
@@ -0,0 +1,12 @@
+//the function base code.
+function number(string, numberOfCharacters) {
+  //made a variable to show the final results.
+  let output = 0;
+  //at first removing the spaces (using replaceAll method and replacing each space with nothing.). then extracting the targeted part(using substring method.)
+  output = string.replaceAll(" ", "").substr(0, numberOfCharacters);
+  //at the end returning the main var.
+  return output;
+}
+//testing.
+console.log(number("ak-47 the arabic muslim loves to hooootoootototototoooooo", 20));
+
